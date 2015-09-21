cecbd9e8d400f575f8122a517fb27277ff9c65b6
Pavlo Gorb <p.orbitum@gmail.com>
2015-09-21 15:03:35 +0300

    - Fixed misspelling for VCVirgilVersion header in the umbrella header.

--- Wrapper/VirgilCryptoiOS.h
+++ Wrapper/VirgilCryptoiOS.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 VirgilSecurity. All rights reserved.
 //
 
-#import "VCVersion.h"
+#import "VCVirgilVersion.h"
 #import "VCKeyPair.h"
 #import "VCCryptor.h"
 #import "VCSigner.h"
