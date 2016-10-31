159b292a8f49ddfaea249e834433ae759a27c296
Oleksandr Deundiak <Deundiak@gmail.com>
2016-10-31 11:34:51 +0200

    Improved gitignore. Removed all targerts. Added iOS Framework target

---
+++ Application/VirgilCrypto/VirgilCrypto.h
@@ -0,0 +1,19 @@
+//
+//  VirgilCrypto.h
+//  VirgilCrypto
+//
+//  Created by Oleksandr Deundiak on 10/31/16.
+//  Copyright © 2016 VirgilSecurity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VirgilCrypto.
+FOUNDATION_EXPORT double VirgilCryptoVersionNumber;
+
+//! Project version string for VirgilCrypto.
+FOUNDATION_EXPORT const unsigned char VirgilCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VirgilCrypto/PublicHeader.h>
+
+
