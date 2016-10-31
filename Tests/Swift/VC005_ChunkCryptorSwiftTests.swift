2d7e30d0e5a4d4ea17477288d89e03ab95ebeabf
Oleksandr Deundiak <Deundiak@gmail.com>
2016-10-31 19:47:28 +0200

    Fixed swift tests

--- Tests/Swift/VC005_ChunkCryptorSwiftTests.swift
+++ Tests/Swift/VC005_ChunkCryptorSwiftTests.swift
@@ -37,197 +37,84 @@ class VC005_ChunkCryptorSwiftTests: XCTestCase {
         let keyPair = VSCKeyPair()
         // Generate a public key id
         let recipientId = UUID().uuidString
-        // Encrypt:
         // Create a cryptor instance
         let cryptor = VSCChunkCryptor()
         // Add a key recepient to enable key-based encryption
         do {
             try cryptor.addKeyRecipient(recipientId.data(using: .utf8)!, publicKey: keyPair.publicKey(), error: ())
         }
-        catch let error as NSError {
+        catch {
             print("Error adding key recipient: \(error.localizedDescription)")
             XCTFail()
         }
         
-        var actualSize:Int = 0
-        let encryptedData = NSMutableData()
-        do {
-            var error: NSError? = nil
-
-            //actualSize = cryptor.startEncryption(withPreferredChunkSize: kDesiredDataChunkLength, error: &error)
-            if let err = error {
-                XCTFail("Error starting chunk encryption: \(err.localizedDescription)")
-            }
-            
-            
-            for offset in stride(from:0, to: self.toEncrypt.count, by: actualSize) {
-                let bytesPtr = self.toEncrypt.withUnsafeBytes({ (bytes:UnsafePointer<CChar>) -> UnsafePointer<CChar> in
-                    return bytes + offset
-                })
-                
-                let mutBytes = UnsafeMutablePointer(mutating: bytesPtr)
-                let chunk = Data(bytesNoCopy: UnsafeMutablePointer(mutBytes), count: Int(actualSize), deallocator: .none)
-                //let encryptedChunk = try cryptor.processDataChunk(chunk)
-               // encryptedData.append(encryptedChunk)
-            }
-        }
-        catch let error as NSError {
-            XCTFail("Error during chunk encryption: \(error.localizedDescription)")
-        }
-        XCTAssertTrue(encryptedData.length > 0, "Encrypted data should contain actual data.");
+        let istream = InputStream(data: self.toEncrypt)
+        let ostream = OutputStream.toMemory()
         
-        do {
-           // try cryptor.finish()
-        }
-        catch let error as NSError {
-            XCTFail("Error finishing chunks processing: \(error.localizedDescription)")
+        var error: NSError?
+        cryptor.encryptData(from: istream, to: ostream, preferredChunkSize: kDesiredDataChunkLength, embedContentInfo: true, error: &error)
+        
+        if error != nil {
+            XCTFail()
         }
         
-        var contentInfo = Data()
-        do {
-            contentInfo = try cryptor.contentInfoWithError()
-        }
-        catch let error as NSError {
-            XCTFail("Error getting content info from cryptor: \(error.localizedDescription)")
-        }
-        XCTAssertTrue(contentInfo.count > 0, "Content Info should contain necessary information.");
+        let encryptedData = ostream.property(forKey: .dataWrittenToMemoryStreamKey) as! Data
+        XCTAssert(encryptedData.count > 0)
         
         let decryptor = VSCChunkCryptor()
         
-        do {
-            try decryptor.setContentInfo(contentInfo, error: ())
-        }
-        catch let error as NSError {
-            XCTFail("Error setting content info: \(error.localizedDescription)")
+        let idecstream = InputStream(data: encryptedData)
+        let odecstream = OutputStream.toMemory()
+        
+        decryptor.decrypt(from: idecstream, to: odecstream, recipientId: recipientId.data(using: .utf8)!, privateKey: keyPair.privateKey(), keyPassword: nil, error: &error)
+        if error != nil {
+            XCTFail()
         }
         
-        actualSize = 0
-        let plainData = NSMutableData()
-        do {
-            var error: NSError? = nil
-           // actualSize = decryptor.startDecryption(withRecipientId: recipientId, privateKey: keyPair.privateKey(), keyPassword: nil, error: &error)
-            if let err = error {
-                XCTFail("Error starting chunk decryption: \(err.localizedDescription)")
-            }
-            
-            for offset in stride(from:0, to: self.toEncrypt.count, by: actualSize) {
-                let bytesPtr = encryptedData.bytes + offset
-                let mutBytes = UnsafeMutableRawPointer(mutating: bytesPtr)
-                let chunk = Data(bytesNoCopy: UnsafeMutableRawPointer(mutBytes), count: Int(actualSize), deallocator: .none)
-            //    let decryptedChunk = try decryptor.processDataChunk(chunk)
-            //    plainData.append(decryptedChunk)
-            }
-        }
-        catch let error as NSError {
-            XCTFail("Error during chunk decryption: \(error.localizedDescription)")
-        }
-        XCTAssertTrue(plainData.length > 0, "Decrypted data should contain actual data.")
+        let plainData = odecstream.property(forKey: .dataWrittenToMemoryStreamKey) as! Data
         
-        do {
-            //try decryptor.finish()
-        }
-        catch let error as NSError {
-            XCTFail("Error finishing chunks processing: \(error.localizedDescription)")
-        }
-        
-        XCTAssertEqual(plainData as Data, self.toEncrypt, "Initial data and decrypted data should be equal.")
+        XCTAssert(plainData == self.toEncrypt)
     }
     
     func test003_passwordBasedEncryptDecrypt() {
-        // Encrypt:
-        let password = "secret"
-        // Encrypt:
+        let passwd = "secret"
         // Create a cryptor instance
         let cryptor = VSCChunkCryptor()
         // Add a key recepient to enable key-based encryption
         do {
-            try cryptor.addPasswordRecipient(password, error: ())
+            try cryptor.addPasswordRecipient(passwd, error: ())
         }
-        catch let error as NSError {
-            print("Error adding password recipient: \(error.localizedDescription)")
+        catch {
+            print("Error adding key recipient: \(error.localizedDescription)")
             XCTFail()
         }
         
-        var actualSize = 0
-        let encryptedData = NSMutableData()
-        do {
-            var error: NSError? = nil
-           // actualSize = cryptor.startEncryption(withPreferredChunkSize: kDesiredDataChunkLength, error: &error)
-            if let err = error {
-                XCTFail("Error starting chunk encryption: \(err.localizedDescription)")
-            }
-            
-            for offset in stride(from:0, to: self.toEncrypt.count, by: actualSize) {
-                let bytesPtr = self.toEncrypt.withUnsafeBytes({ (bytes:UnsafePointer<CChar>) -> UnsafePointer<CChar> in
-                    return bytes + offset
-                })
-                
-                let mutBytes = UnsafeMutablePointer(mutating: bytesPtr)
-                let chunk = Data(bytesNoCopy: UnsafeMutablePointer(mutBytes), count: Int(actualSize), deallocator: .none)
-             //   let encryptedChunk = try cryptor.processDataChunk(chunk)
-              //  encryptedData.append(encryptedChunk)
-            }
-        }
-        catch let error as NSError {
-            XCTFail("Error during chunk encryption: \(error.localizedDescription)")
-        }
-        XCTAssertTrue(encryptedData.length > 0, "Encrypted data should contain actual data.");
+        let istream = InputStream(data: self.toEncrypt)
+        let ostream = OutputStream.toMemory()
         
-        do {
-           // try cryptor.finish()
-        }
-        catch let error as NSError {
-            XCTFail("Error finishing chunks processing: \(error.localizedDescription)")
+        var error: NSError?
+        cryptor.encryptData(from: istream, to: ostream, preferredChunkSize: kDesiredDataChunkLength, embedContentInfo: true, error: &error)
+        
+        if error != nil {
+            XCTFail()
         }
         
-        var contentInfo = Data()
-        do {
-            contentInfo = try cryptor.contentInfoWithError()
-        }
-        catch let error as NSError {
-            XCTFail("Error getting content info from cryptor: \(error.localizedDescription)")
-        }
-        XCTAssertTrue(contentInfo.count > 0, "Content Info should contain necessary information.");
+        let encryptedData = ostream.property(forKey: .dataWrittenToMemoryStreamKey) as! Data
+        XCTAssert(encryptedData.count > 0)
         
         let decryptor = VSCChunkCryptor()
         
-        do {
-            try decryptor.setContentInfo(contentInfo, error: ())
-        }
-        catch let error as NSError {
-            XCTFail("Error setting content info: \(error.localizedDescription)")
+        let idecstream = InputStream(data: encryptedData)
+        let odecstream = OutputStream.toMemory()
+        
+        decryptor.decrypt(from: idecstream, to: odecstream, password: passwd, error: &error)
+        if error != nil {
+            XCTFail()
         }
         
-        actualSize = 0
-        let plainData = NSMutableData()
-        do {
-            var error: NSError? = nil
-          //  actualSize = decryptor.startDecryption(withPassword: password, error: &error)
-            if let err = error {
-                XCTFail("Error starting chunk decryption: \(err.localizedDescription)")
-            }
-            
-            for offset in stride(from:0, to: self.toEncrypt.count, by: actualSize) {
-                let bytesPtr = encryptedData.bytes + offset
-                let mutBytes = UnsafeMutableRawPointer(mutating: bytesPtr)
-                let chunk = Data(bytesNoCopy: UnsafeMutableRawPointer(mutBytes), count: Int(actualSize), deallocator: .none)
-            //    let decryptedChunk = try decryptor.processDataChunk(chunk)
-            //    plainData.append(decryptedChunk)
-            }
-        }
-        catch let error as NSError {
-            XCTFail("Error during chunk decryption: \(error.localizedDescription)")
-        }
-        XCTAssertTrue(plainData.length > 0, "Decrypted data should contain actual data.")
+        let plainData = odecstream.property(forKey: .dataWrittenToMemoryStreamKey) as! Data
         
-        do {
-           // try decryptor.finish()
-        }
-        catch let error as NSError {
-            XCTFail("Error finishing chunks processing: \(error.localizedDescription)")
-        }
-        
-        XCTAssertEqual(plainData as Data, self.toEncrypt, "Initial data and decrypted data should be equal.")
+        XCTAssert(plainData == self.toEncrypt)
     }
     
     func randomDataWithBytes(_ length: Int) -> Data {
