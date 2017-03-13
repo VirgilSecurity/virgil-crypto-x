ad1224d89c21a3115594bd0d67b9d1c40c80192e
Oleksandr Deundiak <Deundiak@gmail.com>
2017-03-13 17:02:32 +0200

    Created UmbrellaHeader. Added VirgilAsymmetricCipher to umbrella header

---
+++ Source/VSCUmbrellaHeader.h
@@ -0,0 +1,9 @@
+//
+//  VSCUmbrellaHeader.h
+//  VirgilCrypto
+//
+//  Created by Oleksandr Deundiak on 3/13/17.
+//  Copyright © 2017 VirgilSecurity. All rights reserved.
+//
+
+#import <VSCCrypto/VirgilAsymmetricCipher.h>
