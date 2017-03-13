d1906a0be87c381e44158a684ddf1272e20ca49e
Oleksandr Deundiak <Deundiak@gmail.com>
2017-03-13 17:58:22 +0200

    Revert "Created UmbrellaHeader. Added VirgilAsymmetricCipher to umbrella header"
    This reverts commit ad1224d89c21a3115594bd0d67b9d1c40c80192e.

--- Source/VSCUmbrellaHeader.h
+++
@@ -1,9 +0,0 @@
-//
-//  VSCUmbrellaHeader.h
-//  VirgilCrypto
-//
-//  Created by Oleksandr Deundiak on 3/13/17.
-//  Copyright © 2017 VirgilSecurity. All rights reserved.
-//
-
-#import <VSCCrypto/VirgilAsymmetricCipher.h>
