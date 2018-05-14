cf5281d753d3cef832b4d3aef5f9cbd2d6aa7424
Oleksandr Deundiak <Deundiak@gmail.com>
2018-05-14 19:04:18 +0300

    Updated version tests

--- Tests/VirgilCrypto/VSC000_VirgilVersion.swift
+++ Tests/VirgilCrypto/VSC000_VirgilVersion.swift
@@ -40,11 +40,11 @@ import XCTest
 
 class VSC000_VirgilVersion: XCTestCase {
     func test001() {
-        XCTAssert(VirgilVersion.asNumber() == 2 << 16 | 3 << 8 | 0)
-        XCTAssert(VirgilVersion.asString() == "2.3.0")
+        XCTAssert(VirgilVersion.asNumber() == 2 << 16 | 4 << 8 | 3)
+        XCTAssert(VirgilVersion.asString() == "2.4.3")
         XCTAssert(VirgilVersion.majorVersion() == 2)
         XCTAssert(VirgilVersion.minorVersion() == 3)
         XCTAssert(VirgilVersion.patchVersion() == 0)
-        XCTAssert(VirgilVersion.fullName() == "2.3.0")
+        XCTAssert(VirgilVersion.fullName() == "2.3.4")
     }
 }
