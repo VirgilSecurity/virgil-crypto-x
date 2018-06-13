df81e4ac26ebe6d4403f5f5ea337b83289d1557e
Oleksandr Deundiak <Deundiak@gmail.com>
2018-06-13 13:11:15 +0300

    Fixed Xcode 10 support

--- Scripts/move_framework.sh
+++ Scripts/move_framework.sh
@@ -1,40 +1,34 @@
+diff(){
+  awk 'BEGIN{RS=ORS=" "}
+       {NR==FNR?a[$0]++:a[$0]--}
+       END{for(k in a)if(a[k])print k}' <(echo -n "${!1}") <(echo -n "${!2}")
+}
+
 rm -rf "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework"
 
 case "${SWIFT_PLATFORM_TARGET_PREFIX}" in
     "ios")
         cp -R "VSCCrypto/PrebuiltFramework/iOS/${PRODUCT_NAME}.framework" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework"
-
-        if [ "${arch}" == "arm64" ]
-        then
-           lipo -remove i386 -output "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
-           lipo -remove x86_64 -output "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
-        else
-           lipo -remove arm64 -output "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
-           lipo -remove armv7 -output "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
-           lipo -remove armv7s -output "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
-        fi
     ;;
     "macosx")
         cp -R "VSCCrypto/PrebuiltFramework/macOS/${PRODUCT_NAME}.framework" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework"
     ;;
-    "tvos")
-        cp -R "VSCCrypto/PrebuiltFramework/tvOS/${PRODUCT_NAME}.framework" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework"
-
-        if [ "${arch}" == "arm64" ]
-        then
-           lipo -remove x86_64 -output "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
-        else
-           lipo -remove arm64 -output "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
-        fi
-    ;;
-    "watchos")
-        cp -R "VSCCrypto/PrebuiltFramework/watchOS/${PRODUCT_NAME}.framework" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework"
-
-        if [ "${arch}" == "armv7k" ]
-        then
-           lipo -remove i386 -output "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
-        else
-           lipo -remove armv7k -output "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
-        fi
-    ;;
 esac
+
+LIPO_OUTPUT="$(lipo -info "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}")"
+echo "LIPO_OUTPUT: ${LIPO_OUTPUT}"
+PREFIX="Architectures in the fat file: ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME} are: "
+LIPO_CLEAN_OUTPUT=${LIPO_OUTPUT#$PREFIX}
+echo "LIPO_CLEAN_OUTPUT: ${LIPO_CLEAN_OUTPUT}"
+INCLUDED_ARCHS=( $LIPO_CLEAN_OUTPUT )
+echo "INCLUDED_ARCHS: ${INCLUDED_ARCHS[@]}"
+echo "VALID_ARCHS: ${ARCHS[@]}"
+
+ARCHS_TO_EXCLUDE=$(diff INCLUDED_ARCHS[@] VALID_ARCHS[@])
+echo "ARCHS_TO_EXCLUDE: ${ARCHS_TO_EXCLUDE[@]}}"
+
+for EXCLUDE_ARCH in ${ARCHS_TO_EXCLUDE[@]}
+do
+  echo "Excluding ${EXCLUDE_ARCH}"
+  lipo -remove $EXCLUDE_ARCH -output "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
+done
