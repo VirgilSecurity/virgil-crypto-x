7fb407aa8f1b7561d5a0555933995575b83f3af9
Oleksandr Deundiak <deundiak@gmail.com>
2019-01-18 18:08:24 +0200

    Added empty project

---
+++ VirgilCryptoApiImpl/VirgilCryptoApiImpl.h
@@ -0,0 +1,19 @@
+//
+//  VirgilCryptoApiImpl.h
+//  VirgilCryptoApiImpl
+//
+//  Created by Oleksandr Deundiak on 1/18/19.
+//  Copyright © 2019 Oleksandr Deundiak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VirgilCryptoApiImpl.
+FOUNDATION_EXPORT double VirgilCryptoApiImplVersionNumber;
+
+//! Project version string for VirgilCryptoApiImpl.
+FOUNDATION_EXPORT const unsigned char VirgilCryptoApiImplVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VirgilCryptoApiImpl/PublicHeader.h>
+
+
