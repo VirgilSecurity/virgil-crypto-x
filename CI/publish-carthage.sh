9f6286c0476ee1df8384e52b7f623489da44c56b
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-07-20 22:59:34 +0300

    Updated travis.yml

---
+++ CI/publish-carthage.sh
@@ -0,0 +1,8 @@
+brew update;
+brew outdated carthage || brew upgrade carthage;
+carthage build --use-xcframeworks --no-skip-current;
+
+# TODO: Should be replaced by carthage archive, when it supports xcframeworks
+FRAMEWORKS_PATH=Carthage/Build
+find ${FRAMEWORKS_PATH} ! -name 'VirgilCrypto.xcframework' -delete
+zip -r VirgilCrypto.xcframework.zip ${FRAMEWORKS_PATH}
