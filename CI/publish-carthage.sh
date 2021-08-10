afab5700a1e517d60a6fd734adb772790f9a9f78
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-08-10 22:45:45 +0300

    Fixed typo

--- CI/publish-carthage.sh
+++ CI/publish-carthage.sh
@@ -3,4 +3,4 @@ brew outdated carthage || brew upgrade carthage;
 carthage build --use-xcframeworks --no-skip-current;
 
 # TODO: Should be replaced by carthage archive, when it supports xcframeworks
-zip -r VirgilSDK.xcframework.zip Carthage/Build/VirgilCrypto.xcframework
+zip -r VirgilCrypto.xcframework.zip Carthage/Build/VirgilCrypto.xcframework
