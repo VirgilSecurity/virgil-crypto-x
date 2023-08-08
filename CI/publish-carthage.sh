7a01ab1eded3eea26b3a0409f900a8fdbd8155a7
SergeySeroshtan <sergey.seroshtan@gmail.com>
2023-08-07 22:49:25 -0400

    Update underlying crypto to 0.17.0-dev.2
    Remove Carthage.
    Use SPM dependencies instead of Carthage for Xcode project.

--- CI/publish-carthage.sh
+++
@@ -1,4 +0,0 @@
-carthage build --use-xcframeworks --no-skip-current;
-
-# TODO: Should be replaced by carthage archive, when it supports xcframeworks
-zip -r VirgilCrypto.xcframework.zip Carthage/Build/VirgilCrypto.xcframework
